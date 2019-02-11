2f3c012a3323a86af57ba13d1fb4180d9185cab7
Ayme Arango <arangoayme@gmail.com>
2019-02-11 15:56:30 -0300

    initial commit

--- Model2_Experiments/Experiment_1.py
+++ Model2_Experiments/Experiment_1.py
@@ -18,8 +18,10 @@ def run_model_exp1(oversampling_rate, vector_type, embed_size,flag):
     else:
         model_type = "blstm"
     
+    x_text, labels = load_data('train')
+        
     #Oversampling before cross-validation
-    x_text, labels = get_data(oversampling_rate,'train',flag)
+    x_text, labels = oversampling(x_text, labels,oversampling_rate)
     
     #cross-validation with oversampled data
     cv_object = KFold(n_splits=5, shuffle=True, random_state=42)
@@ -37,7 +39,7 @@ def run_model_exp1(oversampling_rate, vector_type, embed_size,flag):
             else:
                 X_test.append(x_text[i])
                 y_test.append(labels[i])
-
+       
         data_dict = data_processor(x_text,X_train,y_train,X_test,y_test,flag)
         precision, recall, f1_score,precisionw, recallw, f1_scorew,precisionm, recallm, f1_scorem = train(data_dict, model_type, vector_type,flag, embed_size)    
         
@@ -53,7 +55,7 @@ def run_model_exp1(oversampling_rate, vector_type, embed_size,flag):
     print_scores(p, p1, r,r1, f1, f11,pn, rn, fn,NO_OF_FOLDS)
     
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Experiment 0. Original Experiment Replica')
+    parser = argparse.ArgumentParser(description='Experiment 1. Original Experiment Replica')
     parser.add_argument('--type', choices=['binary', 'categorical'], default = 'categorical')
     vector_type = "sswe"
     oversampling_rate = 3

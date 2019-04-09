2908298f3791edfe2ede6a7a999f8ff573e8228c
Ayme Arango <arangoayme@gmail.com>
2019-04-08 23:23:41 -0400

    actualizaciones

--- Model1_Experiments/Experiment_1.py
+++ Model1_Experiments/Experiment_1.py
@@ -4,64 +4,63 @@ import numpy as np
 import pdb
 from auxiliares import *
 
-def run_model_exp1(flag, strategy):        
+def run_model_exp1():        
     #Experimento 1 Cross-Validation
-    tweets,_ = select_tweets('waseem',strategy)
+    tweets,_ = select_tweets('waseem',None)
+    
     vocab = gen_vocab(tweets)
-    X, y = gen_sequence(tweets, vocab, flag)
-    #Y = y.reshape((len(y), 1))
-    MAX_SEQUENCE_LENGTH = max(map(lambda x:len(x), X))
-    print ("max seq length is %d"%(MAX_SEQUENCE_LENGTH))
-    data = pad_sequences(X, maxlen=MAX_SEQUENCE_LENGTH)
-    y = np.array(y)
-    data, y = sklearn.utils.shuffle(data, y)
-    W = get_embedding_weights()
-    print(data.shape)
-    if flag == 'binary':
-        model = lstm_model_bin(data.shape[1], EMBEDDING_DIM)
-    else:
-        model = lstm_model(data.shape[1], EMBEDDING_DIM)
-    train_LSTM_variante1(tweets,data, y, model, EMBEDDING_DIM, W, 10, 128,flag)
+    
+    MAX_SEQUENCE_LENGTH = max_len(tweets)
+    
+    train_LSTM_variante1(tweets,vocab, MAX_SEQUENCE_LENGTH)
 
-def train_LSTM_variante1(tweets,X, y, model, inp_dim, weights, epochs, batch_size,flag):
+def train_LSTM_variante1(tweets, vocab, MAX_SEQUENCE_LENGTH):
     #Step 1: Training the embeddings with all data
     a, p, r, f1 = 0., 0., 0., 0.
     a1, p1, r1, f11 = 0., 0., 0., 0.
     pn, rn, fn = 0., 0., 0.
-    sentence_len = X.shape[1]
-    current_fold=0
+
+    X, y = gen_sequence(tweets, vocab, 'categorical' )
+    X = pad_sequences(X, maxlen=MAX_SEQUENCE_LENGTH)
+    X, y = sklearn.utils.shuffle(X, y)
+    y = np.array(y)
+    
     y_train = y.reshape((len(y), 1))
     X_temp = np.hstack((X, y_train))
-    for epoch in range(epochs):
-        for X_batch in batch_gen(X_temp, batch_size):
-            x = X_batch[:, :sentence_len]
-            y_temp = X_batch[:, sentence_len]
+   
+    model = lstm_model(MAX_SEQUENCE_LENGTH, EMBEDDING_DIM)
+    #initializing with random embeddings
+    shuffle_weights(model)
+    
+    for epoch in range(EPOCHS):
+        for X_batch in batch_gen(X_temp, BATCH_SIZE):
+            x = X_batch[:, :MAX_SEQUENCE_LENGTH]
+            y_temp = X_batch[:, MAX_SEQUENCE_LENGTH]
             try:
-                if flag != 'binary':
-                    y_temp = np_utils.to_categorical(y_temp, num_classes=3)
+                y_temp = np_utils.to_categorical(y_temp, num_classes=3)
             except Exception as e:
                 print (e)
             loss, acc = model.train_on_batch(x, y_temp, class_weight=None)
+
+  
+    tweets,_ = select_tweets('waseem', None)
+        
     #Extracting learned embeddings 
     wordEmb = model.layers[0].get_weights()[0]
   
     #Step 2: Cross- Validation Using the XGB classifier and the learned embeddings
-    tweets, word2vec_model =select_tweets_whose_embedding_exists('waseem',wordEmb)
-    X, y = gen_data(tweets, word2vec_model,flag)
-    cv_object = StratifiedKFold(n_splits=NO_OF_FOLDS, shuffle=True, random_state=42)
-    for train_index, test_index in cv_object.split(X,y):
-        tweets_train = []
-        tweets_test = []
-        for i in range(len(tweets)):
-            if i in test_index:
-                tweets_test.append(tweets[i])
-            else:
-                tweets_train.append(tweets[i])
-                
-        X_train, y_train = gen_data(tweets_train,word2vec_model,flag)
-        X_test, y_test = gen_data(tweets_test,word2vec_model,flag)
+    word2vec_model = create_model(wordEmb,vocab)
 
-        precision, recall, f1_score, acc, p_weighted, p_macro, r_weighted, r1_macro, f1_weighted, f11_macro = gradient_boosting_classifier(tweets, wordEmb,[],[], X_train, y_train, X_test, y_test,flag)
+    X, y = gen_data(tweets, word2vec_model,'categorical')
+    cv_object = KFold(n_splits=NO_OF_FOLDS, shuffle=True, random_state=42)
+    
+   
+
+    for train_index, test_index in cv_object.split(X):
+        X_train, y_train = X[train_index],y[train_index]
+        X_test, y_test = X[test_index],y[test_index]
+        
+        precision, recall, f1_score, acc, p_weighted, p_macro, r_weighted, r1_macro, f1_weighted, f11_macro = gradient_boosting_classifier(X_train, y_train,X_test, y_test,'categorical')
         a += acc
         p += p_weighted
         p1 += p_macro
@@ -76,22 +75,15 @@ def train_LSTM_variante1(tweets,X, y, model, inp_dim, weights, epochs, batch_siz
     print_scores(p, p1, r,r1, f1, f11,pn, rn, fn, NO_OF_FOLDS)    
     
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='LSTM based models for twitter Hate speech detection')
-    parser.add_argument('-t', '--type',choices=['binary', 'categorical'], default = 'categorical')
     TOKENIZER = 'glove'
     GLOVE_MODEL_FILE = 'glove.txt'
     EMBEDDING_DIM = 200
     OPTIMIZER = 'adam'
-    INITIALIZE_WEIGHTS_WITH = 'random'
     LEARN_EMBEDDINGS = True
     EPOCHS = 10
     BATCH_SIZE = 128
     SCALE_LOSS_FUN = None
+    NO_OF_FOLDS = 10
     SEED = 42
     np.random.seed(SEED)
-    print ('GLOVE embedding: %s' %(GLOVE_MODEL_FILE))
-    print ('Embedding Dimension: %d' %(EMBEDDING_DIM))
-    print ('Allowing embedding learning: %s' %(str(LEARN_EMBEDDINGS)))
-    flag = parser.parse_args().type
-    word2vec_model = gensim.models.KeyedVectors.load_word2vec_format(GLOVE_MODEL_FILE)
-    run_model_exp1(flag, None)
+    run_model_exp1()
--- Model1_Experiments/Experiment_2.py
+++ Model1_Experiments/Experiment_2.py
@@ -5,70 +5,26 @@ import pdb
 from auxiliares import *
 
 
-def run_model_exp2(flag, strategy):        
+def run_model_exp2():        
     #Experimento 1 Cross-Validation "Correctamente"
-    tweets,_ = select_tweets('waseem',strategy)
-    gen_vocab(tweets)
-    X, y = gen_sequence(tweets, vocab, flag)
-    #Y = y.reshape((len(y), 1))
-    MAX_SEQUENCE_LENGTH = max(map(lambda x:len(x), X))
-    print ("max seq length is %d"%(MAX_SEQUENCE_LENGTH))
-
-    data = pad_sequences(X, maxlen=MAX_SEQUENCE_LENGTH)
-    y = np.array(y)
-    data, y = sklearn.utils.shuffle(data, y)
-    W = get_embedding_weights()
-    print(data.shape)
-    if flag == 'binary':
-        model = lstm_model_bin(data.shape[1], EMBEDDING_DIM)
-    else:
-        model = lstm_model(data.shape[1], EMBEDDING_DIM)
-    train_LSTM_OK(tweets, data, y, model, EMBEDDING_DIM, W,10, 128,flag)
+    tweets,_ = select_tweets('waseem',None)
     
-def train_LSTM_OK(tweets,X, y, model, inp_dim, weights, epochs, batch_size,flag):
-    cv_object = StratifiedKFold(n_splits=NO_OF_FOLDS, shuffle=True, random_state=42)
-    print (cv_object)
+    vocab = gen_vocab(tweets)
+
+    MAX_SEQUENCE_LENGTH = max_len(tweets)
+
+    train_LSTM_OK(tweets, vocab, MAX_SEQUENCE_LENGTH)
+    
+def train_LSTM_OK(tweets,vocab, MAX_SEQUENCE_LENGTH):
+    
+    cv_object = KFold(n_splits = NO_OF_FOLDS, shuffle=True, random_state=42)
     a, p, r, f1 = 0., 0., 0., 0.
     a1, p1, r1, f11 = 0., 0., 0., 0.
     pn, rn, fn = 0., 0., 0.
-
-    sentence_len = X.shape[1]
     
-    for train_index, test_index in cv_object.split(X,y):
-        if INITIALIZE_WEIGHTS_WITH == "glove":
-            model.layers[0].set_weights([weights])
-        elif INITIALIZE_WEIGHTS_WITH == "random":
-            shuffle_weights(model)
-        else:
-            print ("ERROR!")
-            return
-        print('train_index')
-        print(len(train_index))
-        print(train_index)
-        X_train, y_train = X[train_index], y[train_index]
-        X_test, y_test = X[test_index], y[test_index]
-        y_train = y_train.reshape((len(y_train), 1))
-        X_temp = np.hstack((X_train, y_train))
-        for epoch in range(epochs):
-            for X_batch in batch_gen(X_temp, batch_size):
-                x = X_batch[:, :sentence_len]
-                y_temp = X_batch[:, sentence_len]
+    model = lstm_model(MAX_SEQUENCE_LENGTH, EMBEDDING_DIM)
 
-                class_weights = None
-                if SCALE_LOSS_FUN:
-                    class_weights = {}
-                    class_weights[0] = np.where(y_temp == 0)[0].shape[0]/float(len(y_temp))
-                    class_weights[1] = np.where(y_temp == 1)[0].shape[0]/float(len(y_temp))
-                    class_weights[2] = np.where(y_temp == 2)[0].shape[0]/float(len(y_temp))
-
-                try:
-                    if flag != 'binary':
-                        y_temp = np_utils.to_categorical(y_temp, num_classes=3)
-                except Exception as e:
-                    print (e)
-                #print (x.shape, y_temp.shape)
-                loss, acc = model.train_on_batch(x, y_temp, class_weight=class_weights)
-        wordEmb = model.layers[0].get_weights()[0]
+    for train_index, test_index in cv_object.split(tweets):
         tweets_train = []
         tweets_test = []
         for i in range(len(tweets)):
@@ -76,11 +32,48 @@ def train_LSTM_OK(tweets,X, y, model, inp_dim, weights, epochs, batch_size,flag)
                 tweets_test.append(tweets[i])
             else:
                 tweets_train.append(tweets[i])
-                
-        X_train, y_train = gen_data(tweets_train,word2vec_model,flag)
-        X_test, y_test = gen_data(tweets_test,word2vec_model,flag)
         
-        precision, recall, f1_score, acc, p_weighted, p_macro, r_weighted, r1_macro, f1_weighted, f11_macro = gradient_boosting_classifier(tweets, wordEmb,train_index,test_index, X_train, y_train, X_test, y_test,flag)
+        shuffle_weights(model)
+            
+        X_train, y_train = gen_sequence(tweets_train, vocab, 'categorical')
+        X_train = pad_sequences(X_train, maxlen=MAX_SEQUENCE_LENGTH)
+        X_train = np.array(X_train)
+        y_train = np.array(y_train)
+        
+        X_test, y_test = gen_sequence(tweets_test, vocab, 'categorical')
+        X_test = pad_sequences(X_test, maxlen=MAX_SEQUENCE_LENGTH)
+        X_test = np.array(X_test)
+        y_test = np.array(y_test)
+
+        
+        y_train = y_train.reshape((len(y_train), 1))
+        X_temp = np.hstack((X_train, y_train))
+        
+        for epoch in range(EPOCHS):
+            for X_batch in batch_gen(X_temp, BATCH_SIZE):
+                x = X_batch[:, :MAX_SEQUENCE_LENGTH]
+                y_temp = X_batch[:, MAX_SEQUENCE_LENGTH]
+
+                class_weights = None
+                
+                y_temp = np_utils.to_categorical(y_temp, num_classes=3)
+
+                loss, acc = model.train_on_batch(x, y_temp, class_weight=class_weights)
+                
+                
+        wordEmb = model.layers[0].get_weights()[0]
+       
+        word2vec_model = create_model(wordEmb,vocab)
+        
+#         tweets_train = select_tweets_whose_embedding_exists(tweets_train, word2vec_model)
+#         tweets_test = select_tweets_whose_embedding_exists(tweets_test, word2vec_model)
+
+        
+        X_train, y_train = gen_data(tweets_train, word2vec_model,'categorical')
+        X_test, y_test = gen_data(tweets_test,word2vec_model,'categorical')
+        
+        precision, recall, f1_score, acc, p_weighted, p_macro, r_weighted, r1_macro, f1_weighted, f11_macro = gradient_boosting_classifier(X_train, y_train, X_test, y_test, 'categorical')
+
         a += acc
         p += p_weighted
         p1 += p_macro
@@ -95,7 +88,6 @@ def train_LSTM_OK(tweets,X, y, model, inp_dim, weights, epochs, batch_size,flag)
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='LSTM based models for twitter Hate speech detection')
-    parser.add_argument('-t', '--type',choices=['binary', 'categorical'], default = 'categorical')
     TOKENIZER = 'glove'
     GLOVE_MODEL_FILE = 'glove.txt'
     EMBEDDING_DIM = 200
@@ -103,13 +95,10 @@ if __name__ == "__main__":
     INITIALIZE_WEIGHTS_WITH = 'random'
     LEARN_EMBEDDINGS = True
     EPOCHS = 10
+    NO_OF_FOLDS = 10
     BATCH_SIZE = 128
     SCALE_LOSS_FUN = None
     SEED = 42
     np.random.seed(SEED)
-    print ('GLOVE embedding: %s' %(GLOVE_MODEL_FILE))
-    print ('Embedding Dimension: %d' %(EMBEDDING_DIM))
-    print ('Allowing embedding learning: %s' %(str(LEARN_EMBEDDINGS)))
-    flag = parser.parse_args().type
-    word2vec_model = gensim.models.KeyedVectors.load_word2vec_format(GLOVE_MODEL_FILE)
-    run_model_exp2(flag, None)
+
+    run_model_exp2()

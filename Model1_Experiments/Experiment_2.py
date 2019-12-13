4db01fbf9f40bd4bbfb3576c2fbfc7a56ccef959
Ayme Arango <arangoayme@gmail.com>
2019-12-12 22:32:38 -0300

    update

--- Model1_Experiments/Experiment_2.py
+++ Model1_Experiments/Experiment_2.py
@@ -3,27 +3,27 @@ from keras.preprocessing.sequence import pad_sequences
 import numpy as np
 import pdb
 from auxiliares import *
+from models import *
 
 
 def run_model_exp2():        
     #Experimento 1 Cross-Validation "Correctamente"
     tweets,_ = select_tweets('waseem',None)
     
-    vocab = gen_vocab(tweets)
-
     MAX_SEQUENCE_LENGTH = max_len(tweets)
 
-    train_LSTM_OK(tweets, vocab, MAX_SEQUENCE_LENGTH)
+    train_LSTM_OK(tweets, MAX_SEQUENCE_LENGTH)
     
-def train_LSTM_OK(tweets,vocab, MAX_SEQUENCE_LENGTH):
+def train_LSTM_OK(tweets, MAX_SEQUENCE_LENGTH):
     
     cv_object = KFold(n_splits = NO_OF_FOLDS, shuffle=True, random_state=42)
     a, p, r, f1 = 0., 0., 0., 0.
     a1, p1, r1, f11 = 0., 0., 0., 0.
     pn, rn, fn = 0., 0., 0.
-    
-    model = lstm_model(MAX_SEQUENCE_LENGTH, EMBEDDING_DIM)
 
+    labels = get_labels(tweets)
+    tweets, labels = sklearn.utils.shuffle(tweets,labels)
+    
     for train_index, test_index in cv_object.split(tweets):
         tweets_train = []
         tweets_test = []
@@ -33,18 +33,17 @@ def train_LSTM_OK(tweets,vocab, MAX_SEQUENCE_LENGTH):
             else:
                 tweets_train.append(tweets[i])
         
-        shuffle_weights(model)
-            
+        vocab = gen_vocab(tweets_train)
+
         X_train, y_train = gen_sequence(tweets_train, vocab, 'categorical')
         X_train = pad_sequences(X_train, maxlen=MAX_SEQUENCE_LENGTH)
+        
         X_train = np.array(X_train)
         y_train = np.array(y_train)
         
-        X_test, y_test = gen_sequence(tweets_test, vocab, 'categorical')
-        X_test = pad_sequences(X_test, maxlen=MAX_SEQUENCE_LENGTH)
-        X_test = np.array(X_test)
-        y_test = np.array(y_test)
-
+    
+        model = lstm_model(MAX_SEQUENCE_LENGTH, EMBEDDING_DIM,vocab)
+        model.layers[0].set_weights([get_embedding_weights2(vocab)])
         
         y_train = y_train.reshape((len(y_train), 1))
         X_temp = np.hstack((X_train, y_train))
@@ -59,32 +58,29 @@ def train_LSTM_OK(tweets,vocab, MAX_SEQUENCE_LENGTH):
                 y_temp = np_utils.to_categorical(y_temp, num_classes=3)
 
                 loss, acc = model.train_on_batch(x, y_temp, class_weight=class_weights)
-                
-                
+
+      #  shuffle_weights(model)
+            
         wordEmb = model.layers[0].get_weights()[0]
        
         word2vec_model = create_model(wordEmb,vocab)
         
-#         tweets_train = select_tweets_whose_embedding_exists(tweets_train, word2vec_model)
-#         tweets_test = select_tweets_whose_embedding_exists(tweets_test, word2vec_model)
-
-        
         X_train, y_train = gen_data(tweets_train, word2vec_model,'categorical')
-        X_test, y_test = gen_data(tweets_test,word2vec_model,'categorical')
+        X_test, y_test = gen_data(tweets_test, word2vec_model,'categorical')
         
-        precision, recall, f1_score, acc, p_weighted, p_macro, r_weighted, r1_macro, f1_weighted, f11_macro = gradient_boosting_classifier(X_train, y_train, X_test, y_test, 'categorical')
-
-        a += acc
-        p += p_weighted
-        p1 += p_macro
-        r += r_weighted
-        r1 += r1_macro
-        f1 += f1_weighted
-        f11 += f11_macro
+        model = gradient_boosting_classifier(X_train, y_train)
+        precision, recall, f1_score,precisionw, recallw, f1_scorew,precisionm, recallm, f1_scorem =evaluate_model(model, X_test, y_test, 'categorical')
+        
+        p += precisionw
+        p1 += precisionm
+        r += recallw
+        r1 += recallm
+        f1 += f1_scorew
+        f11 += f1_scorem
         pn += precision
         rn += recall
         fn += f1_score
-    print_scores(p, p1, r,r1, f1, f11,pn, rn, fn, NO_OF_FOLDS)
+    print_scores(p, p1, r,r1, f1, f11,pn, rn, fn,NO_OF_FOLDS)
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='LSTM based models for twitter Hate speech detection')
